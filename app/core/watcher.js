427ef196c638840d6d6781aef1f99982c7bb9316
soyuka <soyuka@gmail.com>
2013-10-26 19:23:48 +0200

    Added mkdir if tmp doesn't exists

--- app/core/watcher.js
+++ app/core/watcher.js
@@ -173,6 +173,9 @@ var watcher = {
         });
 	},
 	writeRemovedFiles : function(watch) {
+		if(!fs.existsSync(__dirname + '/public/tmp'))
+			fs.mkdirSync(__dirname + '/public/tmp', '0775');
+
 
 		var path = pathInfo.join(__dirname, '/../public/tmp/', watch.uid+'.json');
 
