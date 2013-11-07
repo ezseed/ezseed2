5556b16f7589a463eb52eb0cdf82f137255e341b
soyuka <soyuka@gmail.com>
2013-11-07 17:55:27 +0100

    added dameon fixs

--- app/scripts/rutorrent/daemon.sh
+++ app/scripts/rutorrent/daemon.sh
@@ -45,9 +45,6 @@ options=""
 # default directory for screen, needs to be an absolute path
 base="`su $user -c 'echo ~/'`"
 
-echo $base
-echo $config
-
 # name of screen session
 srnname="rtorrent-$user "
 
@@ -88,11 +85,11 @@ checkcnfg() {
 d_start() {
   [ -d "${base}" ] && cd "${base}"
   stty stop undef && stty start undef
-  su $user -c "screen -ls | grep -sq "\.${srnname}[[:space:]]" " ${user} || su $user -c "screen -dm -S ${srnname} 2>&1 1>/dev/null" ${user} | tee -a "$logfile" >&2
+  su -c "screen -ls | grep -sq "\.${srnname}[[:space:]]" " ${user} || su -c "screen -dm -S ${srnname} 2>&1 1>/dev/null" ${user} | tee -a "$logfile" >&2
   # this works for the screen command, but starting rtorrent below adopts screen session gid
   # even if it is not the screen session we started (e.g. running under an undesirable gid
   #su -c "screen -ls | grep -sq "\.${srnname}[[:space:]]" " ${user} || su -c "sg \"$group\" -c \"screen -fn -dm -S ${srnname} 2>&1 1>/dev/null\"" ${user} | tee -a "$logfile" >&2
-  su $user -c "screen -S "${srnname}" -X screen rtorrent ${options} 2>&1 1>/dev/null" ${user} | tee -a "$logfile" >&2
+  su -c "screen -S "${srnname}" -X screen rtorrent ${options} 2>&1 1>/dev/null" ${user} | tee -a "$logfile" >&2
 }
 
 d_stop() {
