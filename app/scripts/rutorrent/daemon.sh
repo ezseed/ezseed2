06c23f6212b42da58ca6d644312db0e7dcfab0c0
soyuka <soyuka@gmail.com>
2013-11-07 18:23:35 +0100

    added dameon fixs

--- app/scripts/rutorrent/daemon.sh
+++ app/scripts/rutorrent/daemon.sh
@@ -85,11 +85,12 @@ checkcnfg() {
 d_start() {
   [ -d "${base}" ] && cd "${base}"
   stty stop undef && stty start undef
-  su -c "screen -ls | grep -sq "\.${srnname}[[:space:]]" " ${user} || su -c "screen -dm -S ${srnname} 2>&1 1>/dev/null" ${user} | tee -a "$logfile" >&2
-  # this works for the screen command, but starting rtorrent below adopts screen session gid
-  # even if it is not the screen session we started (e.g. running under an undesirable gid
-  #su -c "screen -ls | grep -sq "\.${srnname}[[:space:]]" " ${user} || su -c "sg \"$group\" -c \"screen -fn -dm -S ${srnname} 2>&1 1>/dev/null\"" ${user} | tee -a "$logfile" >&2
-  su -c "screen -S "${srnname}" -X screen rtorrent ${options} 2>&1 1>/dev/null" ${user} | tee -a "$logfile" >&2
+  su $user -c "screen -dmS ${srnname} rtorrent"
+  # su -c "screen -ls | grep -sq "\.${srnname}[[:space:]]" " ${user} || su -c "screen -dm -S ${srnname} 2>&1 1>/dev/null" ${user} | tee -a "$logfile" >&2
+  # # this works for the screen command, but starting rtorrent below adopts screen session gid
+  # # even if it is not the screen session we started (e.g. running under an undesirable gid
+  # #su -c "screen -ls | grep -sq "\.${srnname}[[:space:]]" " ${user} || su -c "sg \"$group\" -c \"screen -fn -dm -S ${srnname} 2>&1 1>/dev/null\"" ${user} | tee -a "$logfile" >&2
+  # su -c "screen -S "${srnname}" -X screen rtorrent ${options} 2>&1 1>/dev/null" ${user} | tee -a "$logfile" >&2
 }
 
 d_stop() {
