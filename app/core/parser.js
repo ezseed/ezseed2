e4f425c608992cf6f0fd19af040da6821333484a
soyuka <soyuka@gmail.com>
2013-11-12 11:39:09 +0100

    fixed VA

--- app/core/parser.js
+++ app/core/parser.js
@@ -61,12 +61,8 @@ module.exports.processAlbums = function(params, callback) {
 					var a = _s.slugify(_s.trim(albums[indexMatch].artist).toLowerCase());
 					var b = _s.slugify(_s.trim(infos.artist).toLowerCase());
 					
-					if(a.indexOf(b) === -1 && b.indexOf(a) === -1) {
-						console.log(a, b);
-						console.log(a.indexOf(b));
+					if(a.indexOf(b) === -1 && b.indexOf(a) === -1)
 						albums[indexMatch].artist = 'VA';
-
-					}
 				}
 
 				albums[indexMatch].songs.push(e);
