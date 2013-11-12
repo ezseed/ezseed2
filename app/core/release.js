ebc1b7926085159c3f5995ba4a37260fe07ceecd
soyuka <soyuka@gmail.com>
2013-11-12 19:45:05 +0100

    ?

--- app/core/release.js
+++ app/core/release.js
@@ -43,9 +43,9 @@ var qualities = ['720p', '1080p', 'cam', 'ts', 'dvdscr', 'r5', 'dvdrip', 'dvdr',
 
 //Dummy name by replacing the founded vars
 var dummyName = function (name, obj) {
-        if(name !== undefined)
-                name = name.replace(obj.quality, '').replace(obj.subtitles, '').replace(obj.language, '').replace(obj.format, '');
-        return _s.trim(name);
+	if(name !== undefined)
+		name = name.replace(obj.quality, '').replace(obj.subtitles, '').replace(obj.language, '').replace(obj.format, '');
+	return _s.trim(name);
 }
 
 
@@ -53,206 +53,205 @@ var dummyName = function (name, obj) {
 * Searches for a cover in a directory
 **/
 var findCoverInDirectory = function(dir) {
-        var files = fs.readdirSync(dir);
-        
-        var m, type, cover;
+	var files = fs.readdirSync(dir);
+	
+	var m, type, cover;
 
-        for(var i in files) {
-                m = mime.lookup(files[i]);
-                type = m.split('/');
-                if(type[0] == 'image') {
-                        cover = files[i];
-                        break;
-                }
-        }
-        
-        return cover === undefined ? null : pathInfos.join(dir, cover).replace(global.config.path, '/downloads');
+	for(var i in files) {
+		m = mime.lookup(files[i]);
+		type = m.split('/');
+		if(type[0] == 'image') {
+			cover = files[i];
+			break;
+		}
+	}
+	
+	return cover === undefined ? null : pathInfos.join(dir, cover).replace(global.config.path, '/downloads');
 }
 
 module.exports.getTags  = {
-        //Searches the video type
-        //Algorithm from : https://github.com/muttsnutts/mp4autotag/issues/2
-        video: function(path) {
+	//Searches the video type
+	//Algorithm from : https://github.com/muttsnutts/mp4autotag/issues/2
+	video: function(path) {
 
-                var basename = pathInfos.basename(path), prevDir = path.replace('/' + basename, '').split('/');
+		var basename = pathInfos.basename(path), prevDir = path.replace('/' + basename, '').split('/');
 
-                prevDir = prevDir[prevDir.length - 1];
+		prevDir = prevDir[prevDir.length - 1];
 
-                if(prevDir.length > basename.length)
-                        basename = prevDir;
+		if(prevDir.length > basename.length)
+			basename = prevDir;
 
-                var err = null
+		var err = null
 
-                  , name = basename.replace(pathInfos.extname(basename), '').replace(/^\-[\w\d]+$/i, '').replace(/\.|\-|_|\(|\)/g, ' ')
+		  , name = basename.replace(pathInfos.extname(basename), '').replace(/^\-[\w\d]+$/i, '').replace(/\.|\-|_|\(|\)/g, ' ')
 
-                  , array = _s.words(name)
+		  , array = _s.words(name)
 
-                  , movie = {
-                                quality : _.find(array, function(v) { return _.contains(qualities, v.toLowerCase()) }),
-                                subtitles : _.find(array, function(v) { return _.contains(subtitles, v.toLowerCase()) }),
-                                language : _.find(array, function(v) { return _.contains(languages, v.toLowerCase()) }),
-                                audio : _.find(array, function(v) { return _.contains(audios, v.toLowerCase()) }),
-                                format : _.find(array, function(v) { return _.contains(format, v.toLowerCase()) }),
-                                movieType : 'movie',
-                        }
-                        
-                  , r = new RegExp(/E[0-9]{1,2}|[0-9]{1,2}x[0-9]{1,2}/i) //searches for the tv show
-                  , y = new RegExp(/([0-9]{4})/) //Year regex
-                  ;
+		  , movie = {
+				quality : _.find(array, function(v) { return _.contains(qualities, v.toLowerCase()) }),
+				subtitles : _.find(array, function(v) { return _.contains(subtitles, v.toLowerCase()) }),
+				language : _.find(array, function(v) { return _.contains(languages, v.toLowerCase()) }),
+				audio : _.find(array, function(v) { return _.contains(audios, v.toLowerCase()) }),
+				format : _.find(array, function(v) { return _.contains(format, v.toLowerCase()) }),
+				movieType : 'movie',
+			}
+			
+		  , r = new RegExp(/E[0-9]{1,2}|[0-9]{1,2}x[0-9]{1,2}/i) //searches for the tv show
+		  , y = new RegExp(/([0-9]{4})/) //Year regex
+		  ;
 
-                //Found a tv show
-                if(r.test(name)) {
+		//Found a tv show
+		if(r.test(name)) {
 
-                        movie.movieType = 'tvseries';
+			movie.movieType = 'tvseries';
 
-                        //Searches for the Season number + Episode number
-                        r = new RegExp(/(.+)S([0-9]+)E([0-9]+)/i);
-                        var r2 = new RegExp(/(.+)([0-9]+)x([0-9])+/i);
+			//Searches for the Season number + Episode number
+			r = new RegExp(/(.+)S([0-9]+)E([0-9]+)/i);
+			var r2 = new RegExp(/(.+)([0-9]+)x([0-9])+/i);
 
-                        var ar = name.match(r);
+			var ar = name.match(r);
 
-                        //If it matches
-                        if(ar != null) {
-                                movie = _.extend(movie, {
-                                        name : _s.trim(ar[1]),
-                                        season : ar[2],
-                                        episode : ar[3]
-                                });
-                        } else {
-                                ar = name.match(r2);
-                                if(ar) {
-                                        movie = _.extend(movie, {
-                                                name: _s.trim(ar[1]),
-                                                season: ar[2],
-                                                episode: ar[3]
-                                        });
-                                } else {
-                                        movie.name = dummyName(name, movie);
-                                }
-                        }
-                } else if(y.test(name)) {
+			//If it matches
+			if(ar != null) {
+				movie = _.extend(movie, {
+					name : _s.trim(ar[1]),
+					season : ar[2],
+					episode : ar[3]
+				});
+			} else {
+				ar = name.match(r2);
+				if(ar) {
+					movie = _.extend(movie, {
+						name: _s.trim(ar[1]),
+						season: ar[2],
+						episode: ar[3]
+					});
+				} else {
+					movie.name = dummyName(name, movie);
+				}
+			}
+		} else if(y.test(name)) {
 
-                        movie.movieType = 'movie';
+			movie.movieType = 'movie';
 
-                        var ar = name.match(y);
+			var ar = name.match(y);
 
-                        //year > 1900
-                        if(ar != null && ar[0] > 1900) {
-                                var parts = name.split(ar[0]);
-                                movie : _.extend(movie, {
-                                        name : _s.trim(parts[0]),
-                                        year : ar[1]
-                                });
-                        } else {
-                                movie.name = dummyName(name, movie);
-                        }
-                } else {
-                        movie.name = dummyName(name, movie);
-                }
+			//year > 1900
+			if(ar != null && ar[0] > 1900) {
+				var parts = name.split(ar[0]);
+				movie : _.extend(movie, {
+					name : _s.trim(parts[0]),
+					year : ar[1]
+				});
+			} else {
+				movie.name = dummyName(name, movie);
+			}
+		} else {
+			movie.name = dummyName(name, movie);
+		}
 
-                console.log(movie);
-                
-                return movie;
-        }, 
-        audio: function(filePath, picture) {
+		return movie;
+	}, 
+	audio: function(filePath, picture) {
 
-                picture = picture === undefined ? false : picture;
+		picture = picture === undefined ? false : picture;
 
-                var id3 = new ID3(fs.readFileSync(filePath));
-                id3.parse();
+		var id3 = new ID3(fs.readFileSync(filePath));
+		id3.parse();
 
-                var tags = {
-                                "title" : id3.get("title"),
-                                "artist" :id3.get("artist"),
-                                "album"  :id3.get("album"),
-                                "year"   :id3.get("year"),
-                                "genre"  :id3.get("genre")
-                        };
+		var tags = {
+				"title" : id3.get("title"),
+				"artist" :id3.get("artist"),
+				"album"  :id3.get("album"),
+				"year"   :id3.get("year"),
+				"genre"  :id3.get("genre")
+			};
 
-                if(picture) {
-                        var datas = id3.get('picture'), pictureFounded = false;
+		if(picture) {
+			var datas = id3.get('picture'), pictureFounded = false;
 
-                        if(datas !== null && (datas.data !== undefined && datas.format !== undefined) ) {
+			if(datas !== null && (datas.data !== undefined && datas.format !== undefined) ) {
 
-                                var coverName = new Buffer(tags.artist + tags.album).toString().replace(/[^a-zA-Z0-9]+/ig,'')
+				var coverName = new Buffer(tags.artist + tags.album).toString().replace(/[^a-zA-Z0-9]+/ig,'')
 
-                                  , file = pathInfos.join(global.config.root, '/public/tmp/') + _.uniqueId('cover' + coverName)
+				  , file = pathInfos.join(global.config.root, '/public/tmp/') + _.uniqueId('cover' + coverName)
 
-                                  , type = datas.format.split('/');
+				  , type = datas.format.split('/');
 
-                                if(type[0] == 'image') {
-                                        pictureFounded = true;
+				if(type[0] == 'image') {
+					pictureFounded = true;
 
-                                        file = file + '.' + type[1];
+					file = file + '.' + type[1];
 
-                                        fs.writeFileSync(file, datas.data);
-                                        
-                                        tags = _.extend(tags, {picture: file.replace(global.config.root + '/public', '')});
-                                }
+					fs.writeFileSync(file, datas.data);
+					
+					tags = _.extend(tags, {picture: file.replace(global.config.root + '/public', '')});
+				}
 
-                        }
-                        
-                        if(!pictureFounded)
-                                _.extend(tags, {picture: findCoverInDirectory(pathInfos.dirname(filePath)) });
-                        
-                        
-                }
+			}
+			
+			if(!pictureFounded)
+				_.extend(tags, {picture: findCoverInDirectory(pathInfos.dirname(filePath)) });
+			
+			
+		}
 
-                return tags;
-        }
+		return tags;
+	}
 };
 
 var getMovieInformations = function(movie, cb) {
 
-        //searching in the allocine API (could be others)
-          allocine.api('search', { q:movie.name, filter: movie.movieType, count: '2'}, function(err, res) {
-                  if(err) return cb(err, movie);
+	//searching in the allocine API (could be others)
+  	allocine.api('search', { q:movie.name, filter: movie.movieType, count: '2'}, function(err, res) {
+  		if(err) return cb(err, movie);
 
-                  if(!_.isUndefined(res.feed)) {
-                      var infos = Object.byString(res.feed, movie.movieType);
+  		if(!_.isUndefined(res.feed)) {
+      		var infos = Object.byString(res.feed, movie.movieType);
 
-                      if(infos !== undefined) {
+      		if(infos !== undefined) {
 
-                              _.each(infos, function(e) {
-                                      if(
-                                              ( e.title !== undefined && movie.name.toLowerCase().indexOf(e.title.toLowerCase()) !== -1 ) 
-                                              ||
-                                              ( e.originalTitle !== undefined && movie.name.toLowerCase().indexOf(e.originalTitle.toLowerCase()) !== -1 )
-                                        )
-                                              movie.code = e.code;
-                              });
+      			_.each(infos, function(e) {
+      				if(
+      					( e.title !== undefined && movie.name.toLowerCase().indexOf(e.title.toLowerCase()) !== -1 ) 
+      					||
+      					( e.originalTitle !== undefined && movie.name.toLowerCase().indexOf(e.originalTitle.toLowerCase()) !== -1 )
+      				  )
+      					movie.code = e.code;
+      			});
 
-                          movie.code = movie.code === undefined ? infos[0].code : movie.code;
+          		movie.code = movie.code === undefined ? infos[0].code : movie.code;
 
-                          //Searching for a specific code
-                          allocine.api(movie.movieType, {code: movie.code}, function(err, result) { 
-                                  infos = Object.byString(result, movie.movieType);
+          		//Searching for a specific code
+          		allocine.api(movie.movieType, {code: movie.code}, function(err, result) { 
+          			infos = Object.byString(result, movie.movieType);
 
-                                  movie.title = infos.title !== undefined ? infos.title : infos.originalTitle;
-                                  movie.synopsis = infos.synopsis ? _s.trim(infos.synopsis.replace(/(<([^>]+)>)/ig, '')) : '';
-                                  movie.picture = infos.poster !== undefined ? infos.poster.href : null;
-                                  movie.trailer = _.isEmpty(infos.trailer) ? null : infos.trailer.href;
+          			movie.title = infos.title !== undefined ? infos.title : infos.originalTitle;
+          			movie.synopsis = infos.synopsis ? _s.trim(infos.synopsis.replace(/(<([^>]+)>)/ig, '')) : '';
+          			movie.picture = infos.poster !== undefined ? infos.poster.href : null;
+          			movie.trailer = _.isEmpty(infos.trailer) ? null : infos.trailer.href;
 
-                                  return cb(err, movie);
+          			return cb(err, movie);
 
-                          });
-                  } else {
-                          var words = _s.words(movie.name);
+          		});
+          	} else {
+          		var words = _s.words(movie.name);
 
-                          if(words.length >= 2 && words[0].length > 3) {
-                                  movie.name = words.splice(1, words.length).join(' ');
-                                  getMovieInformations(movie , cb);
-                          } else {
-                                 //No movie founded
-                                  movie.title = movie.name;
-                                  return cb(err, movie);                          
-                          }
+          		if(words.length >= 2 && words[0].length > 3) {
+          			movie.name = words.splice(1, words.length).join(' ');
+          			getMovieInformations(movie , cb);
+          		} else {
+        			 //No movie founded
+	          		movie.title = movie.name;
+	          		return cb(err, movie);  			
+          		}
 
-                  }
-              } else {
-                      return cb(err, movie);
-              }
-          });
+          	}
+      	} else {
+      		return cb(err, movie);
+      	}
+  	});
 }
 
 module.exports.getMovieInformations = getMovieInformations;
+
--- app/routes/files.js
+++ app/routes/files.js
@@ -1,180 +1,180 @@
 var fs = require('fs')
-        , archiver = require('archiver')
-        , explorer = require('explorer')
-        , async = require('async')
-        , _ = require('underscore')
-        , pathInfo = require('path')
-        , db = require('../core/database.js')
-        , userHelper = require('../core/helpers/users.js');
-        ;
+	, archiver = require('archiver')
+	, explorer = require('explorer')
+	, async = require('async')
+	, _ = require('underscore')
+	, pathInfo = require('path')
+	, db = require('../core/database.js')
+	, userHelper = require('../core/helpers/users.js');
+	;
 
 //To be moved
 var archiveFiles = function(archive, filePaths, callback) {
-        var output = fs.createWriteStream(archive.zip);
-        var zip = archiver('zip');
-        
-        zip.on('error', function(err) {
-                callback(err);
-        });
+	var output = fs.createWriteStream(archive.zip);
+	var zip = archiver('zip');
+	
+	zip.on('error', function(err) {
+		callback(err);
+	});
 
-        zip.pipe(output);
+	zip.pipe(output);
 
 
-        async.each(filePaths, function(item, cb) {
+	async.each(filePaths, function(item, cb) {
 
-                zip.append(
-                        fs.createReadStream(pathInfo.normalize(item)), 
-                        { name: pathInfo.basename(pathInfo.dirname(item)) + '/' + pathInfo.basename(item) },  //, store: true
-                        function(err) {
-                                cb(err);
-                        }
-                );
-        }, function(err){
-           if(err) callback(err);
-           zip.finalize(function(err, written) {
-                        if (err) callback(err);
+		zip.append(
+			fs.createReadStream(pathInfo.normalize(item)), 
+			{ name: pathInfo.basename(pathInfo.dirname(item)) + '/' + pathInfo.basename(item) },  //, store: true
+			function(err) {
+				cb(err);
+			}
+		);
+	}, function(err){
+	   if(err) callback(err);
+	   zip.finalize(function(err, written) {
+			if (err) callback(err);
 
-                        callback(null);
-                });
-        });
+			callback(null);
+		});
+	});
 
 }
 
 var files = {
-        download : function(req, res) {
-                //to do add db Files search.
-                db.files.byId(req.params.id, function(err, doc) {
-                        if(req.params.fid !== undefined) {
-                                var file = db.file.byId(doc, req.params.fid);
+	download : function(req, res) {
+		//to do add db Files search.
+		db.files.byId(req.params.id, function(err, doc) {
+			if(req.params.fid !== undefined) {
+				var file = db.file.byId(doc, req.params.fid);
 
-                                res.download(file.path);
-                        } else {
-                                var files = doc.songs || doc.videos || doc.files;
+				res.download(file.path);
+			} else {
+				var files = doc.songs || doc.videos || doc.files;
 
-                                res.download(files[0].path);
-                        }
-                });
-
-
-        },
-
-        downloadArchive : function(req, res) {
-
-                db.files.byId(req.params.id, function(err, doc) {
-                        var name = '';
-
-                        if(doc.songs !== undefined) {
-                                name += (doc.artist !== undefined && doc.artist !== null) ? doc.artist : '';
-                                name += (doc.album !== undefined && doc.album !== null) ? ' - ' + doc.album : '';
-                                if(name.length == 0) {
-                                        var t = doc.prevDir.split('/');
-                                        t = t[t.length-1];
-
-                                        name += t;
-                                }
-                        } else if(doc.videos !== undefined) {
-                                name += doc.name;
-                                name += !_.isEmpty(doc.season) ? ' S'+doc.season : '';
-                        } else {
-                                name += doc.name;
-                        }
-
-                        if(_.isUndefined(name)) {
-                                req.session.error = 'Aucun fichier trouvé';
-                                res.redirect('/');
-                        } else {
-                                res.download(global.config.root + '/public/tmp/' + req.params.id +'.zip', name + '.zip');
-                        }
-                });
-                                        
-        },
-        //Take _id as archive name + no tmp/user only 1 tmp
-        //To be improved
-        archive : function(req, res) {
-                var archive = {};
+				res.download(files[0].path);
+			}
+		});
 
 
-                db.files.byId(req.params.id, function(err, doc) {
+	},
 
-                        archive.path = doc.prevDir;
+	downloadArchive : function(req, res) {
 
-                        if(_.isUndefined(archive.path)) {
-                                //sends json error
-                                res.json({'error':'Aucun fichier trouvé'});
-                        } else {
-                                
-                                archive.zip = pathInfo.join(global.config.root, 'public/tmp/', req.params.id +'.zip');
+		db.files.byId(req.params.id, function(err, doc) {
+			var name = '';
 
-                                fs.exists(archive.zip, function (exists) {
-                                        if(exists) {
-                                                //sends json redirect download
-                                                res.json({'error':null, 'download':true});
+			if(doc.songs !== undefined) {
+				name += (doc.artist !== undefined && doc.artist !== null) ? doc.artist : '';
+				name += (doc.album !== undefined && doc.album !== null) ? ' - ' + doc.album : '';
+				if(name.length == 0) {
+					var t = doc.prevDir.split('/');
+					t = t[t.length-1];
 
-                                        //If it's a tvserie we need the videos paths
-                                        } else if(!_.isEmpty(doc.season)) {
-                                                
-                                                var filePaths = [];
+					name += t;
+				}
+			} else if(doc.videos !== undefined) {
+				name += doc.name;
+				name += !_.isEmpty(doc.season) ? ' S'+doc.season : '';
+			} else {
+				name += doc.name;
+			}
 
-                                                for(var k in doc.videos)
-                                                        filePaths.push(doc.videos[k].path);
+			if(_.isUndefined(name)) {
+				req.session.error = 'Aucun fichier trouvé';
+				res.redirect('/');
+			} else {
+				res.download(global.config.root + '/public/tmp/' + req.params.id +'.zip', name + '.zip');
+			}
+		});
+					
+	},
+	//Take _id as archive name + no tmp/user only 1 tmp
+	//To be improved
+	archive : function(req, res) {
+		var archive = {};
 
-                                                archive.path = doc.name + ' - ' + doc.season;
 
-                                                archiveFiles(archive, filePaths, function(err) {
-                                                        if(err)
-                                                                res.json({'error' : 'Erreur zip :' + err});
-                                                        else
-                                                                res.json({'error':null, 'download':true});
-                                                });
+		db.files.byId(req.params.id, function(err, doc) {
 
-                                        } else {
+			archive.path = doc.prevDir;
 
-                                                explorer.getFiles(archive.path, function(err, filePaths) {
-                                                        if(err)
-                                                                res.json({'error' : 'Aucun fichiers trouvés'});
-                                                        else {
-                                                                archiveFiles(archive, filePaths, function(err) {
-                                                                        res.json({'error':null, 'download':true});
-                                                                });
-                                                        }
-                                                });
-                                                
-                                        }
-                                });
-                        }
-                });
+			if(_.isUndefined(archive.path)) {
+				//sends json error
+				res.json({'error':'Aucun fichier trouvé'});
+			} else {
+				
+				archive.zip = pathInfo.join(global.config.root, 'public/tmp/', req.params.id +'.zip');
 
-        },
+				fs.exists(archive.zip, function (exists) {
+					if(exists) {
+						//sends json redirect download
+						res.json({'error':null, 'download':true});
 
-        delete : function(req, res) {
-                db.paths.byUser(req.session.user._id, function(err, paths) {
-                        db.files.byId(req.params.id, function(err, doc) {
+					//If it's a tvserie we need the videos paths
+					} else if(!_.isEmpty(doc.season)) {
+						
+						var filePaths = [];
 
-                                var files = doc.files || doc.videos || doc.songs;
+						for(var k in doc.videos)
+							filePaths.push(doc.videos[k].path);
 
-                                db.files[req.params.type + 's'].delete(doc._id, function() {
+						archive.path = doc.name + ' - ' + doc.season;
 
-                                        //Only unlink files, watcher'll do the rest
-                                        _.each(files, function(e) {
-                                                fs.unlinkSync(e.path);
-                                        });
+						archiveFiles(archive, filePaths, function(err) {
+							if(err)
+								res.json({'error' : 'Erreur zip :' + err});
+							else
+								res.json({'error':null, 'download':true});
+						});
 
-                                        //remove prevDir (safe?)
-                                        if(typeof files.prevDir == "string" && paths.paths.indexOf(files.prevDir) === -1)
-                                                fs.rmdirSync(files.prevDir);
+					} else {
 
-                                        //remove tmp
-                                        if(files.picture)
-                                                fs.unlinkSync(files.picture);
+						explorer.getFiles(archive.path, function(err, filePaths) {
+							if(err)
+								res.json({'error' : 'Aucun fichiers trouvés'});
+							else {
+								archiveFiles(archive, filePaths, function(err) {
+									res.json({'error':null, 'download':true});
+								});
+							}
+						});
+						
+					}
+				});
+			}
+		});
 
-                                        req.session.success = doc.name + " a été supprimé avec succès !";
-                                        res.redirect('/#'+doc._id);
-                                });
+	},
 
-                        });
-                });
+	delete : function(req, res) {
+		db.paths.byUser(req.session.user._id, function(err, paths) {
+			db.files.byId(req.params.id, function(err, doc) {
 
-        }
+				var files = doc.files || doc.videos || doc.songs;
+
+				db.files[req.params.type + 's'].delete(doc._id, function() {
+
+					//Only unlink files, watcher'll do the rest
+					_.each(files, function(e) {
+						fs.unlinkSync(e.path);
+					});
+
+					//remove prevDir (safe?)
+					if(typeof files.prevDir == "string" && paths.paths.indexOf(files.prevDir) === -1)
+						fs.rmdirSync(files.prevDir);
+
+					//remove tmp
+					if(files.picture)
+						fs.unlinkSync(files.picture);
+
+					req.session.success = doc.name + " a été supprimé avec succès !";
+					res.redirect('/#'+doc._id);
+				});
+
+			});
+		});
+
+	}
 }
 
 
