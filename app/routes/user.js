989b4ecef3dea109072661d3ff9c18c01df242f5
soyuka <soyuka@gmail.com>
2013-11-29 19:24:08 +0100

    user password

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,5 +1,6 @@
 var _ = require('underscore')
 	, db = require('../core/database')
+	, path = require('path')
 	, userHelper = require('../core/helpers/users.js');
 
 var user = {
@@ -78,7 +79,7 @@ var user = {
 	},
 	password : function(req, res) {
 		db.user.byId(req.params.uid, function(err, user) {
-			var shell_path = pathInfo.resolve(global.config.root, '..', 'ezseed');
+			var shell_path = path.resolve(global.config.root, '..', 'ezseed');
 				
 			var options = ['password', user.client, user.username, ,'-p', req.body.password];
 
