c319fb6472da9e9dd10846238e2d604a42f6d6a0
soyuka <soyuka@gmail.com>
2014-02-18 16:10:56 +0100

    added start check

--- scripts/ezseed.sh
+++ scripts/ezseed.sh
@@ -28,9 +28,14 @@ super() {
 }
 
 start() {
-    echo "Starting $NAME"
-    super $PM2 resurrect
-    super ezseed reboot
+    if[ -n $('ps -ef | grep "pm2: ezseed" | grep -v grep') ]
+    then
+        echo "Ezseed is running"
+    else
+        echo "Starting $NAME"
+        super $PM2 resurrect
+        super ezseed reboot
+    fi
 }
 
 stop() {
