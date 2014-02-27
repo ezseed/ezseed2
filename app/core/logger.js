fc8261e0db587ba90cb5a87966d410e833b686f7
soyuka <soyuka@gmail.com>
2014-02-27 08:36:43 +0100

    Added logger

--- app/core/logger.js
+++ app/core/logger.js
@@ -1,120 +1,34 @@
-var level   = process.argv.indexOf('-d') === -1 ? 6 : 7;
-
-if(level == 6 && process.env.NODE_ENV == 'production') {
-	level = 5;
-}
-
-var logger  = require('caterpillar').createLogger({level:level})
-  , filter  = require('caterpillar-filter').createFilter()
-  , human   = require('caterpillar-human').createHuman({spacer: "  "});
-
-logger.pipe(filter).pipe(human).pipe(process.stdout);
-
-logger = require('underscore').extend(logger, 
-	{
-		error: function() {
-			var args = [].splice.call(arguments, 0);
-			args.unshift('error');
-			this.log.apply(this, args);
-		},
-		warn: function() {
-			var args = [].splice.call(arguments, 0);
-			args.unshift('warn');
-			this.log.apply(this, args);
-		},
-		info: function() {
-			var args = [].splice.call(arguments, 0);
-			args.unshift('info');	
-			this.log.apply(this, args);
-		},
-		debug: function() {
-			var args = [].splice.call(arguments, 0);
-			args.unshift('debug');
-			this.log.apply(this, args);
-		},
-		trace: function() {
-			var args = [].splice.call(arguments, 0);
-
-			console.trace(args);
-			this.log.apply(this, args);
-		},
-		alert: function() {
-			var args = [].splice.call(arguments, 0);
-			args.unshift('alert');
-			this.log.apply(this, args);
-		}
-	}
-);
-
-/**
- * var winston = require('winston'), _ = require('underscore');
-
-// var syslogConfig = {};
-
-// syslogConfig.levels = {
-//   emerg: 0,
-//   alert: 1,
-//   crit: 2,
-//   error: 3,
-//   warning: 4,
-//   notice: 5,
-//   info: 6,
-//   debug: 7,
-// };
-
-// syslogConfig.colors = {
-//   emerg: 'red',
-//   alert: 'yellow',
-//   crit: 'red',
-//   error: 'red',
-//   warning: 'red',
-//   notice: 'yellow',
-//   info: 'green',
-//   debug: 'blue'
-// };
- 
-
-// var winston_config = winston.config.syslog;
-
 var level   = process.argv.indexOf('-d') === -1 ? 'info' : 'debug';
 
-if(level == 6 && process.env.NODE_ENV == 'production') {
+if(level == 'info' && process.env.NODE_ENV == 'production')
 	level = 'notice';
-}
 
+var winston = require('winston');
 
-// winston.addColors(winston.config.syslog.colors);
-
-// var logger = new (winston.Logger)({
-// 	transports: [
-// 	  new (winston.transports.Console)({ level: 'debug', colorize: true, levels: winston.config.syslog.levels, emitErrs: true }),
-// 	  new (winston.transports.File)({ 
-// 	  								   filename: global.config.root + '/exceptions.log'
-// 	  	                             , handleExceptions: true 
-// 	  	                             , levels: winston.config.syslog.levels
-// 	  	                             , level: level
-// 	  	                           })
-// 	]
-// });
-
-var logger = new (winston.Logger)({ level: 'debug', colorize: true, levels: winston.config.syslog.levels, emitErrs: true });
-
-logger.on('error', function (err) {
-	console.log(err.message);
-	// if(err.indexOf('Unknown log error')) {
-	// 	console.log(message);
-	// }
+var logger = new (winston.Logger)({
+	transports: [
+	  new (winston.transports.Console)({ level: level, colorize: true, levels: winston.config.syslog.levels }),
+	  new (winston.transports.File)({ 
+	  								   filename: global.config.root + '/exceptions.log'
+	  	                             , handleExceptions: true 
+	  	                             , levels: winston.config.syslog.levels
+	  	                             , level: level
+	  	                           })
+	]
 });
 
-// logger.log = function() {
-// 	console.log(this.info);
-// 	var args = Array.prototype.slice.call(arguments);
-	
-// 	this.info.apply(null, args);
-// 	// return logger.apply(logger, args);
-// }
+process.on('uncaughtException', function ( err ) {
 
-module.exports = logger;
- */
+    logger.log('alert', err.message);
+    logger.log('alert', err.stack);
+
+    if(err.code == 'MODULE_NOT_FOUND')
+    	logger.log('notice', 'Please try : npm install', function () {
+	      process.exit(1);
+
+      	});
+    else
+    	process.exit();
+});
 
 module.exports = logger;
