4ccfee393cc05c532cd125cc1698d0f231b59fef
soyuka <soyuka@gmail.com>
2014-03-01 20:49:19 +0100

    Added transmission check if running

--- scripts/transmission/daemon.sh
+++ scripts/transmission/daemon.sh
@@ -2,6 +2,15 @@
 opt=$1
 username=$2
 
-/etc/init.d/transmission-daemon-$username $opt
+if [ ! -z $(ps -ef | grep transmission-daemon-$username | grep -v grep) && $opt eq 'start' ];
+then
+	echo "Transmission is already running"
+	exit 1
+else if [ $opt eq 'start' ];
+	/etc/init.d/transmission-daemon-$username $opt
+
+else
+	/etc/init.d/transmission-daemon-$username $opt
+fi
 
 exit 0
