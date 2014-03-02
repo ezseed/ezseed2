3e518a61fc0427d7b5360dc462ad6783a9010fcc
soyuka <soyuka@gmail.com>
2014-03-02 20:44:58 +0100

    Deleted *

--- app/scripts/transmission/daemon.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-username=$2
-opt=$1
-
-/etc/init.d/transmission-daemon-$username $opt
-
-exit 0
--- app/scripts/transmission/password.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-appdir="$(cd $DIR && cd ../../ && pwd)"
-
-usermod -p $(mkpasswd -H md5 "$2") $1
-
-exit 1
