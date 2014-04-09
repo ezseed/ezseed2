85ea97b77b20c84e8886d62a75c86d81af530b7e
soyuka <soyuka@gmail.com>
2014-04-09 09:07:32 +0200

    Getting rtorrent/libtorrent from sources

--- scripts/rutorrent/install.sh
+++ scripts/rutorrent/install.sh
@@ -1,6 +1,6 @@
-##!/bin/bash
+#!/bin/bash
 #su $user -c 'screen -dmS rtorrent rtorrent' lancer
-###
+#
 #Vérification du root
 if [ "$(id -u)" != "0" ]; then
         echo "This bash script needs a root account" 1>&2
@@ -14,15 +14,12 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ##building tools##
 apt-get -y install libncurses5-dev libxmlrpc-c3-dev libcurl3-dev automake libncurses5 libcppunit-dev libtool pkg-config subversion php5-cli unzip ffmpeg curl php5-curl mediainfo screen unrar-free libsigc++-2.0-dev
 
-#comerr-dev libcurl3-openssl-dev libidn11-dev libkadm55 libkrb5-dev libssl-dev zlib1g-dev libncurses5 libncurses5-dev
-
 #Téléchargement + déplacement de rutorrent (web)
 svn checkout http://rutorrent.googlecode.com/svn/trunk/rutorrent/
 svn checkout http://rutorrent.googlecode.com/svn/trunk/plugins/
 mv ./plugins/* ./rutorrent/plugins/
 rm -R ./plugins
 mv rutorrent/ /var/www
-
 
 # get xmlrpc from svn
 svn checkout http://svn.code.sf.net/p/xmlrpc-c/code/stable xmlrpc-c
