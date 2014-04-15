4777b27ce4b30b273c0521c71daed57fb08f2224
hillafen <hillafen@users.noreply.github.com>
2014-04-15 04:55:40 +0200

    Update useradd.sh
    Utilisateur créer même si le répertoire par défaut n'est pas /home + modification des droits des fichiers pour plus de sécurités

--- scripts/rutorrent/useradd.sh
+++ scripts/rutorrent/useradd.sh
@@ -4,60 +4,55 @@
 #Gaaa - LND#
 #Mods by soyuka for ezssed2#
 ############
+###############################################################################
+######################    DECLARATION DES VARIABLES    ########################
+###############################################################################
+USER=$1
+PW=$2
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CHEMIN=$(sudo -i -u $USER env | grep HOME | sed 's/\HOME=//')
+WEBUSER=$(ps aux | grep $(netstat -tulpn | grep :80 | awk -F/ '{print $2}' | sed -e "s/ *$//" | sort -u) | cut -d ' ' -f 1 | sed '/root/d' | sort -u)
 
-###
-#Vérification du root
+###############################################################################
+###############################    SCRIPT    ##################################
+###############################################################################
+# Vérification du root
 if [ "$(id -u)" != "0" ]; then
         echo "This bash script needs a root account" 1>&2
         exit 1
 fi
-###
 
-#On initialise les variables
-#Utilisateur - password
-
-USER=$1
-PW=$2
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-############
-#Creation de l'utilisateur
+# Création de l'utilisateur
 python $DIR/htpasswd.py -b /usr/local/nginx/rutorrent_passwd $USER $PW
-#could root it, see chroot
-su $USER -c 'mkdir -p ~/downloads ~/uploads ~/incomplete ~/rtorrent ~/rtorrent/session'
-#Fin
-##########
 
-##########
-#On met la conf rTorrent
-su $USER -c "touch /home/$USER/.rtorrent.rc"
-cat > /home/$USER/.rtorrent.rc<< EOF
-execute = {sh,-c,rm -f /home/$USER/rtorrent/session/rpc.socket}
-scgi_local = /home/$USER/rtorrent/session/rpc.socket
-execute = {sh,-c,chmod 0666 /home/$USER/rtorrent/session/rpc.socket}
+# Création des répertoires
+chown -R $USER:$USER $CHEMIN
+su $USER -c 'mkdir -p ~/downloads ~/uploads ~/incomplete ~/rtorrent ~/rtorrent/session'
+
+# On met la conf rtorrent
+su $USER -c "touch $CHEMIN/.rtorrent.rc"
+cat > $CHEMIN/.rtorrent.rc<< EOF
+execute = {sh,-c,rm -f $CHEMIN/rtorrent/session/rpc.socket}
+scgi_local = $CHEMIN/rtorrent/session/rpc.socket
+execute = {sh,-c,chmod 0666 $CHEMIN/rtorrent/session/rpc.socket}
 encoding_list = UTF-8
 system.umask.set = 022
+port_range = 45000-65000
 port_random = yes
 check_hash = no
-directory = /home/$USER/incomplete
-session = /home/$USER/rtorrent/session
+directory = $CHEMIN/incomplete
+session = $CHEMIN/rtorrent/session
 encryption = allow_incoming, try_outgoing, enable_retry
 trackers.enable = 1
 use_udp_trackers = yes
 EOF
 
-chown -R $USER  /home/$USER
-
-#Fin
-##########
-
-##########
-#On fait la conf ruTorrent
-mkdir /var/www/rutorrent/conf/users/$USER
+# On fait la conf rutorrent
+mkdir -p /var/www/rutorrent/conf/users/$USER
 cat > /var/www/rutorrent/conf/users/$USER/config.php<< EOF
 <?php
 \$scgi_port = 0;
-\$scgi_host = "unix:///home/$USER/rtorrent/session/rpc.socket";
+\$scgi_host = "unix://$CHEMIN/rtorrent/session/rpc.socket";
 \$XMLRPCMountPoint = "/RPC00001";
 \$pathToExternals = array(
     "php"   => '',               
@@ -66,14 +61,13 @@ cat > /var/www/rutorrent/conf/users/$USER/config.php<< EOF
     "id"    => '',               
     "stat"  => '/usr/bin/stat',  
 );
-\$topDirectory = "/home/$USER";
+\$topDirectory = "$CHEMIN";
 ?>
 EOF
-chmod -R 777 /var/www/rutorrent/
+
+chown -R $WEBUSER:$WEBUSER /var/www/rutorrent
+chmod -R 777 /var/www/rutorrent/share/users
 
 $DIR/daemon.sh start $USER
-
-#Fin du script
-##########
 
 exit 0
